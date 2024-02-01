0cb50e9eff0f142f11264423c5a7030a4ad64a94
Yashaswini <yashaswinipannem0204@gmail.com>
2024-02-01 15:50:31 +0530

    First commit

---
+++ app/src/main/java/com/capgemini/newsapp/NewsInterface.kt
@@ -0,0 +1,24 @@
+package com.capgemini.newsapp
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsInterface {
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(@Query("country")code:String,
+                        @Query("apiKey")key:String):NewsResult
+    companion object{
+        val BASE_URL="https://newsapi.org/v2/"
+
+        fun getInstance():NewsInterface{
+            val retrofitInstance=Retrofit.Builder()//creating instance
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())//Gson does parsing
+                .build()
+
+            return retrofitInstance.create(NewsInterface::class.java)//create
+
+        }
+    }
+}
